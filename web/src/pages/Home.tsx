f311447e1d4e87bb5ee39f80479267895aa1844c
basharkhan7776 <basharkhan7776@gmail.com>
2025-03-04 13:08:57 +0530

    update Home page layout, add Roboto font support, and modify CSS variables

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 function Home() {
   return (
-    <div className="w-full h-screen flex justify-center items-center"><Button>Get Started</Button></div>
+    <div className="font-roboto text-3xl">Hello whats up guys</div>
   )
 }
 

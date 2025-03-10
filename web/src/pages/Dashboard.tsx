de0f1a88b694947d6054845b0fee0a7f3bfa3a7d
basharkhan7776 <basharkhan7776@gmail.com>
2025-03-10 16:28:29 +0530

    add Dashboard and Marketplace pages with new layout components; enhance input functionality and styling

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,4 +1,8 @@
+import { FooterMain } from "@/components/footer"
 import { NavBarDash } from "@/components/navBar"
+import { Card, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { PlusIcon } from "lucide-react"
 
 
 
@@ -6,6 +10,42 @@ export default function Dashboard() {
   return (
     <div className="w-full h-screen">
         <NavBarDash currentPage={"dashboard"}/>
+        <div className="min-h-screen w-full grid grid-cols-2 gap-5 mt-20 py-5 px-48">
+          <Card id="CropDistribution" className="p-2 px-4">
+            <CardTitle className="text-lg font-semibold">Crop Distribution</CardTitle>
+          </Card>
+          <Card id="YieldTrend" className="p-2 px-4">
+            <CardTitle className="text-lg font-semibold">Yield Trend</CardTitle>
+          </Card>
+          <Card id="AiRecommendation" className="p-2 px-4">
+            <CardTitle className="text-lg font-semibold">AI Recommendation</CardTitle>
+          </Card>
+          <Card id="FieldHealthMap" className="p-2 px-4">
+            <CardTitle className="text-lg font-semibold">Field Health Map</CardTitle>
+          </Card>
+          <Card id="MarketPrice" className="p-2 px-4">
+            <CardTitle className="text-lg font-semibold">Market Price</CardTitle>
+          </Card>
+          <Card id="CommunityUpdates" className="p-2 px-4">
+            <CardTitle className="text-lg font-semibold">Community Updates</CardTitle>
+          </Card>
+          <Card id="AiChatBot" className="p-2 px-4">
+            <CardTitle className="text-lg font-semibold">AI Chat Bot</CardTitle>
+          </Card>
+          <Card id="SoilHealth" className="p-2 px-4">
+            <CardTitle className="text-lg font-semibold">Soil Health</CardTitle>
+          </Card>
+          <Card id="IrrigationSchedule" className="p-2 px-4">
+            <CardTitle className="text-lg font-semibold">Irrigation Schedule</CardTitle>
+          </Card>
+          <Card id="PestControlSuggestions" className="p-2 px-4">
+            <CardTitle className="text-lg font-semibold">Pest Control Suggestions</CardTitle>
+          </Card>
+          <div className="col-span-2 flex justify-center items-center">
+            <Button className="h-16 w-32"><PlusIcon /> Add More</Button>
+          </div>
+        </div>
+        <FooterMain/>
     </div>
   )
 }
--- web/src/pages/Marketplace.tsx
+++ web/src/pages/Marketplace.tsx
@@ -1,10 +1,39 @@
 import { NavBarDash } from "@/components/navBar";
+import { FooterMain } from "@/components/footer";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Filter, Heart, History, ShoppingCart } from "lucide-react";
 
 
 export default function Marketplace() {
   return (
     <div className="w-full h-screen">
-        <NavBarDash currentPage={"marketplace"}/>
+      <NavBarDash currentPage={"marketplace"} />
+      <div className="min-h-screen w-full mt-20 p-4 px-12">
+        <div className="flex w-full flex-col gap-4">
+          <div className="flex justify-between items-center gap-12">
+            <Input placeholder="Search product, seed, tools"></Input>
+            <div className="flex gap-4 items-center">
+              <Button variant={"secondary"}><Heart/> Wishlist</Button>
+              <Button variant={"secondary"}><ShoppingCart/> Cart</Button>
+              <Button variant={"secondary"}><History/> History</Button>
+            </div>
+          </div>
+          <div className="flex gap-4">
+            <Button variant={"secondary"}><Filter/> All Categories</Button>
+            <Button variant={"secondary"}>Price Range</Button>
+            <Button variant={"secondary"}>Location</Button>
+            <Button variant={"secondary"}>Sort By</Button>
+          </div>
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+      </div>
+      <FooterMain />
     </div>
   )
 }

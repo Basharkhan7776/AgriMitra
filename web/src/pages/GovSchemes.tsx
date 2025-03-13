977867f2eeb0daa4b2336da576fc01693e0732ef
basharkhan7776 <basharkhan7776@gmail.com>
2025-03-13 12:14:29 +0530

    Inital Ui development complete for backend  ,add GovSchemes and Insurance pages with card components; implement search and filter functionality

--- web/src/pages/GovSchemes.tsx
+++ web/src/pages/GovSchemes.tsx
@@ -1,5 +1,6 @@
 import { NavBarCommunity } from "@/components/navBar";
 import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { ChevronDown, Ellipsis } from "lucide-react";
 
@@ -13,12 +14,29 @@ export default function GovSchemes() {
         <Button variant={"secondary"}>All Regions<ChevronDown/></Button>
         <Button variant={"secondary"}>All Types<ChevronDown/></Button>
       </div>
-      <div>
-
+      <div className="grid grid-cols-5 gap-4">
+        <GovSchemesCard
+          image="https://static.vecteezy.com/system/resources/previews/045/868/113/non_2x/picture-symbol-icon-vector.jpg"
+          title="Title"
+          description="Description"
+        />
       </div>
       <div className="flex justify-center">
         <Button size={"lg"}><Ellipsis/> Load More</Button>
       </div>
     </div>
   )
+}
+
+function GovSchemesCard({title, description, image}:{title:string,description:string,image:string}) {
+  return <Card className="flex flex-col gap-4">
+    <div className="h-1/2 flex justify-center bg-white rounded-lg"> 
+      <img src={image} alt="Image" />
+    </div>
+    <div className="h-1/2 flex flex-col justify-between p-2">
+      <CardTitle>{title}</CardTitle>
+      <CardDescription className="text-lg">{description}</CardDescription>
+      <Button>Learn More</Button>
+    </div>
+  </Card>
 }
--- web/src/pages/Insurance.tsx
+++ web/src/pages/Insurance.tsx
@@ -1,10 +1,48 @@
 import { NavBarCommunity } from "@/components/navBar";
+import { Button } from "@/components/ui/button";
+import { Card, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Check, ChevronDown } from "lucide-react";
 
 
 export default function Insurance() {
   return (
     <div className="min-h-screen w-full mt-20 p-4 px-12 flex flex-col gap-6">
       <NavBarCommunity currentPage={"insurance"}/>
+      <div className="flex gap-6">
+        <Input placeholder="Search insurance plans..."></Input>
+        <Button variant={"secondary"}>All Types<ChevronDown/></Button>
+        <Button variant={"secondary"}>Premimum<ChevronDown/></Button>
+      </div>
+      <div className="grid grid-cols-4 gap-4">
+        <InsuranceCard
+          title="Title"
+          price="1000"
+          lists={["feature 1", "feature 2", "feature 3"]}
+        />
+      </div>
     </div>
   )
+}
+
+function InsuranceCard({title, price, lists}:{title:string, price:string, lists: string[]}) {
+  return <Card className="flex flex-col gap-4 p-4">
+    <CardTitle>{title}</CardTitle>
+    <div>
+      {lists.map((list) => (
+        <div className="flex gap-2 text-wrap" key={list}>
+          <Check className="text-primary"/>
+          <p>{list}</p>
+        </div>
+      ))}
+    </div>
+    <div className="flex justify-between items-center">
+      <div>
+        <Label className="text-gray-700">Starting from</Label>
+        <h1 className="font-semibold">₹{price}/month</h1>
+      </div>
+      <Button>Learn More</Button>
+    </div>
+  </Card>
 }

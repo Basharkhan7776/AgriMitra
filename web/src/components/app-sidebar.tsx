8ce8a8acf8c0f7e4cab264b955670b409e35d434
basharkhan7776 <basharkhan7776@gmail.com>
2025-04-03 13:35:06 +0530

    Add close icon to AppSidebar button for improved user experience

--- web/src/components/app-sidebar.tsx
+++ web/src/components/app-sidebar.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { X } from "lucide-react";
 
 
 
@@ -6,7 +7,7 @@ export function AppSidebar({ toggle, setToggle }: { toggle: boolean, setToggle: 
   return (
     <div className={`${toggle ? "" : "hidden"} w-[300px] h-screen fixed z-20 top-0 right-0 bg-red-400`}>
       AppSidebar
-      <Button variant="secondary" onClick={() => setToggle(false)} className="absolute top-4 right-4"></Button>
+      <Button variant="secondary" onClick={() => setToggle(false)} className="absolute top-4 right-4"><X/></Button>
     </div>
   )
 }
